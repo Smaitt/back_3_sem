723553e688d4a3b4ad64ef3a8c9e7fc1ae5a9be2
Smaitt <114274109+Smaitt@users.noreply.github.com>
2023-09-26 19:28:13 +0600

    Delete helpdesk directory

--- helpdesk/tickets/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.tickets_list, name='tickets_list'),
-    path('add/', views.add_ticket, name='add_ticket'),
-]
